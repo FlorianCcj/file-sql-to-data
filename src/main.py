496224a749468d88cfead1d1dda3c84997fb2c93
florianccj <palmade.florian@gmail.com>
2017-10-28 13:57:13 +0200

    [+] [unique] unique fini, primary key en cours

--- src/main.py
+++ src/main.py
@@ -38,10 +38,14 @@ class DataGenerator:
 		self.data = {}
 		self.unique = {}
 		self.uniqueDatas = {}
+		self.primaryColumn = {}
+		self.timeout = 50
 
 	def launch(self):
+		print('### Lancement de la generation de données ###')
 		if (self.jsonFile):
 			self.dataSchema = self.readJsonFile(self.jsonFile)
+			self.primaryColumn = self.extractPrimaryColumn(self.dataSchema);
 			self.data = self.generateDatas(self.dataSchema)
 			# self.generateJsonFile('data.json', self.data)
 			return self.data
@@ -51,17 +55,14 @@ class DataGenerator:
 			return self.dataSchema
 			if (not self.dataSchema):
 				print ('fichier impossible a parser, verifier qu\'il sagit bien d\'un fichier sql en entree')
-			# else:
-				# self.generateJsonFile(self.outputFile, self.dataSchema)
 
 	def readFileSql(self, inputFile):
+		print('### Lecture du fichier SQL ###')
 		totalRequest = ''
 		try:
 			with open(inputFile, 'r') as file:
 				for line in file:
 					totalRequest += line 
-					# WIP
-					# print(line)
 		except FileNotFoundError as e:
 			print('Le fichier {} n\'existe pas !'.format(e.filename))
 			exit(1)
@@ -74,6 +75,7 @@ class DataGenerator:
 		return totalRequest
 
 	def readJsonFile(self, jsonFile):
+		print('### Lecture du fichier JSON ###')
 		try:
 			with open(jsonFile, 'r') as file:
 				dataSchema = json.load(file)
@@ -84,46 +86,50 @@ class DataGenerator:
 		return dataSchema
 
 	def extractCreateRequests(self, request):
+		print('### Recuperation des differente CREATE TABLE ###')
 		data = {}
-		tablePattern = r'CREATE TABLE ([a-z]+) \((.+)\).*;'
+		tablePattern = r'CREATE TABLE ([^ ]+) \((.+)\).*;'
 		tableMatch = re.findall(tablePattern, request)
 		for table in tableMatch:
-			tableName = table[0]
+
+			tableName = table[0].strip()
 			tableRequest = table[1]
 			data[tableName] = {}
 			data[tableName]['columns'] = {}
 			data[tableName]['numberToCreate'] = 10
-			columnMatch = tableRequest.split(",")
+			columnPattern = r'(?!\([^ )]+),(?! ?[^ (]+\))'
+			columnMatch = re.split(columnPattern, tableRequest) 
 			data[tableName]['columns'] = self.extractColumnsFromCreateRequest(columnMatch, tableName)
 		return data
 
-	def extractColumnsFromCreateRequest(self, columnTable, tableName):
-		columnPattern = r'(\S+) ([^ (]+)(?: ?\(([^)]+)\))? ?(.+)?'
+	def extractColumnsFromCreateRequest(self, columnRequestTable, tableName):
+		print('### Recuperation des columns au sein de la CREATE TABLE %s ###'% tableName)
+		columnPattern = r'([^ ]+) ([^ (]+)(?: ?\(([^)]+)\))? ?(.+)?'
 		columnData = {}
-		for column in columnTable:
-			match = re.findall(columnPattern, column)
+		for columnRequest in columnRequestTable:
+			match = re.findall(columnPattern, columnRequest)
 			if match:
-				columName = match[0][0]
+				columName = match[0][0].strip()
 				columnData[columName] = {} if (not columnData.has_key(columName)) else columnData[columName]
-				if (columName.lower().strip().find('primary') != -1): # est-ce qu il faudrait pas le mettre sur column au lieu de columnName
+				if (columnRequest.lower().strip().find('primary') != -1): # est-ce qu il faudrait pas le mettre sur column au lieu de columnName
 					del columnData[columName]
 					patternPrimaryKey = r'PRIMARY KEY ?\((.+)\)'
-					matchPrimaryColumn = re.findall(patternPrimaryKey, column)
+					matchPrimaryColumn = re.findall(patternPrimaryKey, columnRequest)
 					if(len(matchPrimaryColumn) > 0):
 						primaryKeys = matchPrimaryColumn[0].split(",")
 						for key in primaryKeys:
-							# print(key)
-							if (not columnData.has_key(key)):
-								columnData[key] = {}
-							columnData[key]['primary'] = True
+							cleanKey = key.strip()
+							if (not columnData.has_key(cleanKey)):
+								columnData[cleanKey] = {}
+							columnData[cleanKey]['primary'] = True
 				elif (columName.lower().strip().find('index') != -1):
 					del columnData[columName]
 					pass
 					# todo
-				elif (column.lower().strip().find('unique') != -1):
+				elif (columnRequest.lower().strip().find('unique') != -1):
 					uniquePattern = r'\((.*)\)'
-					matchUnique = re.findall(uniquePattern, column)
-					uniqueColumnName = matchUnique[0]
+					matchUnique = re.findall(uniquePattern, columnRequest)
+					uniqueColumnName = matchUnique[0].strip()
 					if (not self.unique.has_key(tableName)):
 						self.unique[tableName] = {}
 					if (not self.unique[tableName].has_key(uniqueColumnName)):
@@ -135,7 +141,11 @@ class DataGenerator:
 					columnType = match[0][1]
 					columnData[columName]['name'] = columName
 					columnData[columName]['type'] = columnType if columnType else None
-					columnData[columName]['nullable'] = (column.lower().find('not null') != -1)
+					columnData[columName]['nullable'] = (columnRequest.lower().find('not null') != -1)
+					if (not columnData[columName].has_key('unique')):
+						columnData[columName]['unique'] = False
+					if (not columnData[columName].has_key('primary')):
+						columnData[columName]['primary'] = False
 					try:
 						columnData[columName]['size'] = int(match[0][2])
 					except Exception as e:
@@ -144,18 +154,32 @@ class DataGenerator:
 		return columnData
 
 	def isThisTableSColumnsUnique(self, tableName, tableColumns, uniqueTable):
+		print('### Recuperation des colonnes Uniques ###')
 		for column in tableColumns:
 			if (uniqueTable.has_key(tableName)):
 				if uniqueTable[tableName].has_key(column):
-					tableColumns[column]['unique'] = True
+					tableColumns[column.strip()]['unique'] = True
 				else:
-					tableColumns[column]['unique'] = False
+					tableColumns[column.strip()]['unique'] = False
 			else:
-				tableColumns[column]['unique'] = False
+				tableColumns[column.strip()]['unique'] = False
 		return tableColumns
 			# check il il est dans le tablea self.unique
 
+	def extractPrimaryColumn(self, dataSchema):
+		print('### Recuperation des primary key ###')
+		primaryColumn = {}
+		for tableName in dataSchema:
+			tableName = tableName.strip()
+			primaryColumn[tableName] = []
+			for columName in dataSchema[tableName]['columns']:
+				if dataSchema[tableName]['columns'][columName.strip()]['primary']:
+					primaryColumn[tableName].append(columName)
+		print primaryColumn
+		return primaryColumn
+
 	def generateDatas(self, dataSchema):
+		print('### Generation de la data ###')
 		data = {}
 		for tableName in dataSchema:
 			data[tableName] = []
@@ -165,15 +189,31 @@ class DataGenerator:
 		return data
 
 	def generateNewRow(self, columns, numberOfRow, id, tableName):
+		print('### Generation d une entree pour la table %s ###'% tableName)
 		newRow = {}
-		for column in columns:
-			# todo
-			# ici si la colonne est unique alors je stock les valeurs dans le tableau uniqueDatas 
-			# et je recommence la generation aleatoire tant que la valeur retourne est dedans
-			newRow[column] = self.generateDataColumn(columns[column], numberOfRow, id = id)
+		for columnName in columns:
+			if (not self.uniqueDatas.has_key(tableName)):
+				self.uniqueDatas[tableName] = {}
+			if (not self.uniqueDatas[tableName].has_key(columnName)):
+				self.uniqueDatas[tableName][columnName] = {}
+				self.uniqueDatas[tableName][columnName]['unique'] = []
+
+			newData = self.generateDataColumn(columns[columnName], numberOfRow, id = id)
+			if (columns[columnName]['unique']):
+				timeout = 0
+				while ((newData in self.uniqueDatas[tableName][columnName]['unique']) and (timeout < self.timeout)) :
+					newData = self.generateDataColumn(columns[columnName], numberOfRow, id = id)
+					timeout += 1
+				if timeout == self.timeout:
+					print "Tentative de generation de données unique dans la table `%s` pour la colonne `%s` echoué" % tableName, columnName
+			#	print newData
+
+			self.uniqueDatas[tableName][columnName]['unique'].append(newData)
+			newRow[columnName] = newData
 		return newRow
 
 	def generateDataColumn(self, column, numberOfRow, id):
+		print('### Generation d une column d une entree ###')
 		generatedData = None
 		if (column['name'] == 'id'):
 			generatedData = id+1
