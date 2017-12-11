13a41bb316f2d4abb0e3974bf04030d6ac2b2cf2
florianccj <florianccj@gmail.com>
2017-12-11 13:59:34 +0100

    [*][key] ajout du module

---
+++ src/modules/Key.py
@@ -0,0 +1,26 @@
+defaultNumberOfDataToCreate = 'numberOfDataToCreate'
+order = 'order'
+
+indexSearch = "index"
+primaryKeySearch = "primary"
+uniqueSearch = "unique"
+
+index = "index"
+primaryKey = "primary key"
+foreignKey = "foreignKey"
+unique = "unique"
+
+tableName = "tableName"
+columnName = "columnName"
+
+name = "name"
+type = "type"
+nullable = "nullable"
+notNullable = "not null"
+size = "size"
+
+table = "table"
+column = "column"
+columns = "columns"
+
+referenceFile = "refFile"
